a803e4e3791ca5a9df14c3834631b1adf1541f17
tankaye09 <tankaye@gmail.com>
2022-07-02 17:45:56 +0800

    preprocess

--- preprocess.py
+++ preprocess.py
@@ -1,20 +1,69 @@
+import os
 import json
+import string
+import pandas as pd
 import xmltodict
+import nltk
+from nltk.corpus import stopwords, wordnet2021
+from nltk.stem import WordNetLemmatizer
 
-file_path = "sample.xml"
+nltk.download("stopwords")
+nltk.download("omw-1.4")
 
-with open(file_path) as xml_file:
-    data_dict = xmltodict.parse(xml_file.read(), encoding="utf-8")
-    xml_file.close()
+# directory = r"complete_dataset\xml-utf8_with_plots_with_url"
 
-    # generate the object using json.dumps()
-    # corresponding to json data, but as a string, to be stored in a file
+# Using for testing
+directory = r"partial_dataset\xml-utf8_with_plots_with_url"
 
-    json_data = json.dumps(data_dict)
-    print(json.loads(json_data)["doc"]["plot"])
+cachedStopWords = stopwords.words("english")
+lemm = WordNetLemmatizer()
+data_list = []
+for filename in os.listdir(directory):
+    f = os.path.join(directory, filename)
+    # checking if it is a file
+    if os.path.isfile(f):
+        file_path = directory + "\\" + filename
+        with open(file_path) as xml_file:
+            data_dict = xmltodict.parse(xml_file.read(), encoding="utf-8")
+            xml_file.close()
 
-    # # Write the json data to output
-    # # json file
-    # with open("sample.json", "w") as json_file:
-    #     json_file.write(json_data)
-    #     json_file.close()
+        # generate the object using json.dumps()
+        # corresponding to json data, but as a string, to be stored in a file
+
+        json_str = json.dumps(data_dict)
+        json_data = json.loads(json_str)
+
+        # preprocess plot text
+        json_plot = json_data["doc"]["plot"]
+
+        # text lowercase
+        json_plot = str(json_plot).lower()
+
+        # remove punctuation
+        json_plot = json_plot.translate(str.maketrans("", "", string.punctuation))
+
+        # tokenize, lemmatize and remove stopwords
+        json_plot = " ".join(
+            [
+                lemm.lemmatize(word)
+                for word in json_plot.split()
+                if word not in cachedStopWords
+            ]
+        )
+
+        # remove numbers
+        # KIV
+
+        # update json data
+        json_data["doc"]["plot"] = json_plot
+
+        data_list.append(json_data["doc"])
+
+        # Write the json data to output
+        # json file
+        # with open("sample.json", "w", encoding="utf8") as json_file:
+        #     json.dump(json_data, json_file, ensure_ascii=False)
+        #     json_file.close()
+
+df = pd.DataFrame(data_list)
+df.to_csv("processed_data.csv", encoding="utf-8")
