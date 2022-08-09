1ee751c84373bd1951f5c928b48b184f9dfd71b9
Tiong Shan Kai <tiongshankai97@gmail.com>
2022-08-09 14:55:21 +0800

    added react template

---
+++ client/src/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { MantineProvider } from "@mantine/core";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <MantineProvider
+      theme={{ colorScheme: "dark" }}
+      withGlobalStyles
+      withNormalizeCSS
+    >
+      <App />
+    </MantineProvider>
+  </React.StrictMode>
+);
